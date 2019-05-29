6ef9c70705fdd676a568380fcb4fe71cc19d9358
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-05-29 21:20:21 +0900

    Fix Character

--- src/game/character.ts
+++ src/game/character.ts
@@ -1,4 +1,10 @@
 import * as ex from "excalibur";
+import { CoordinatesConverter } from "@/game/coordinates-converter";
+
+export interface CharacterArgs {
+  actor: ex.Actor;
+  coordinatesConverter: CoordinatesConverter;
+}
 
 /**
  * Character explains actor like player character, enemy or etc..
@@ -6,8 +12,18 @@ import * as ex from "excalibur";
 export class Character {
   /** Actor */
   public readonly actor: ex.Actor;
+  private readonly coordinatesConverter: CoordinatesConverter;
 
-  public constructor(actor: ex.Actor) {
-    this.actor = actor;
+  public constructor(args: CharacterArgs) {
+    this.actor = args.actor;
+    this.coordinatesConverter = args.coordinatesConverter;
+  }
+
+  public get location(): ex.Vector {
+    const loc = this.coordinatesConverter.toAreaPoint({
+      x: this.actor.x,
+      y: this.actor.y
+    });
+    return new ex.Vector(loc.x, loc.y);
   }
 }
--- tests/unit/game/character.spec.ts
+++ tests/unit/game/character.spec.ts
@@ -1,16 +1,45 @@
 import * as ex from "excalibur";
-import { Character } from "@/game/character";
-import { simpleMock } from "tests/test-util";
+import { Character, CharacterArgs } from "@/game/character";
+import { CoordinatesConverter } from "@/game/coordinates-converter";
+import { simpleMock } from "../../test-util";
+
+const createMockArgs = (): CharacterArgs => ({
+  actor: simpleMock<ex.Actor>(),
+  coordinatesConverter: simpleMock<CoordinatesConverter>()
+});
 
 describe("Character", (): void => {
   it("has actor", (): void => {
-    // Given actor
-    const actor = simpleMock<ex.Actor>();
+    // Given args
+    const args = createMockArgs();
 
     // When construct character
-    const character = new Character(actor);
+    const character = new Character(args);
 
-    // Then actor has actor
-    expect(character.actor).toBe(actor);
+    // Then character has actor
+    expect(character.actor).toBe(args.actor);
+  });
+
+  it("represent location as area", (): void => {
+    // Given character with args
+    const expectedLoc = { x: 3, y: 4 };
+    const args = createMockArgs();
+    args.actor.x = 1;
+    args.actor.y = 2;
+    args.coordinatesConverter.toAreaPoint = jest
+      .fn()
+      .mockReturnValueOnce(expectedLoc);
+    const character = new Character(args);
+
+    // When get character location in area
+    const location = character.location;
+
+    // Then character deal location from canvas location
+    expect(location.x).toBe(expectedLoc.x);
+    expect(location.y).toBe(expectedLoc.y);
+    expect(args.coordinatesConverter.toAreaPoint).toBeCalledWith({
+      x: args.actor.x,
+      y: args.actor.y
+    });
   });
 });
